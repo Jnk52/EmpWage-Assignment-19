35376887d3a5367c3e50f8711e9de4f41501ac2f
jnk52 <ajienkyanikam52@gmail.com>
2020-05-12 11:05:25 -0700

    uc5

--- empWage.java
+++ empWage.java
@@ -1,14 +1,17 @@
-//EmployeeWageUC4
-public class empWage{
-public static final int IS_PART_TIME=1;
+//EmployeeWageUC5
+  public class empWage{
+  public static final int IS_PART_TIME=1;
   public static final int IS_FULL_TIME=2;
- public static final int EMP_RATE_PER_HOUR=20;
+  public static final int EMP_RATE_PER_HOUR=20;
+  public static final int NUM_OF_WORKING_DAYS=2;
 
 public static void main(String[] args) {
 //Variables
 int empHrs=0;
 int empWage=0;
+int totalEmpWage=0;
 //Computation
+for (int day = 0; day < NUM_OF_WORKING_DAYS; day++){
 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 switch (empCheck) {
 case IS_PART_TIME:
@@ -20,13 +23,12 @@ case IS_FULL_TIME:
 default:
         empHrs=0;
 }
-empWage =empHrs *EMP_RATE_PER_HOUR;
+empWage = empHrs *EMP_RATE_PER_HOUR;
+        totalEmpWage+=empWage;
        System.out.println("Emp Wage: " + empWage);
   }
+  System.out.println("Total Emp Wage: " + totalEmpWage);
 }
-
-
-
-
+}
 
 
