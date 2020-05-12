b6859dbe7ed811665c20c12ab66b3836f3c85690
jnk52 <ajienkyanikam52@gmail.com>
2020-05-12 11:10:13 -0700

    uc6

--- empWage.java
+++ empWage.java
@@ -1,17 +1,21 @@
-//EmployeeWageUC5
+//EmployeeWageUC6
   public class empWage{
   public static final int IS_PART_TIME=1;
   public static final int IS_FULL_TIME=2;
   public static final int EMP_RATE_PER_HOUR=20;
   public static final int NUM_OF_WORKING_DAYS=2;
+  public static final int MAX_HRS_IN_MONTH=10;
 
 public static void main(String[] args) {
 //Variables
-int empHrs=0;
-int empWage=0;
 int totalEmpWage=0;
+int totalWorkingDays=0;
+int totalEmpHrs=0;
 //Computation
-for (int day = 0; day < NUM_OF_WORKING_DAYS; day++){
+while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+        totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        int empHrs=0;
+        totalWorkingDays++;
 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 switch (empCheck) {
 case IS_PART_TIME:
@@ -22,13 +26,13 @@ case IS_FULL_TIME:
         break;
 default:
         empHrs=0;
-}
-empWage = empHrs *EMP_RATE_PER_HOUR;
-        totalEmpWage+=empWage;
-       System.out.println("Emp Wage: " + empWage);
-  }
-  System.out.println("Total Emp Wage: " + totalEmpWage);
-}
-}
+                   }
 
+totalEmpHrs += empHrs;
+int empWage = empHrs * EMP_RATE_PER_HOUR;
+totalEmpWage += empWage;
+}
+System.out.println("Emp Wage: " + totalEmpWage);
+}
+}
 
